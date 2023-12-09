2716ce71616471dbea98a80a67b77a1498566912
Prosti221 <58991997+prosti221@users.noreply.github.com>
2023-12-09 19:10:22 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ pub fn compute_l2(a: &Array<f64, ndarray::Dim<[usize; 1]>>, b: &Array<f64, ndarr
     let mut distance: f64 = 0.0;
     distance = (a - b).mapv(|x| x.powi(2)).sum();
 
-    return distance.sqrt();
+    return distance;
 }
 
 //############### Utility functions #################
